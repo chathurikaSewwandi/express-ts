d9f591f072c218e412e0d2811eb433382fc6f554
csewwa <avcsewwandi@std.ssl.sab.ac.lk>
2025-08-26 17:05:35 +0530

    Add response handling utilities and HTTP status constants

---
+++ src/common/responseHandler.ts
@@ -0,0 +1,11 @@
+import { Response } from 'express';
+import { HttpStatus } from "./constants/httpStatus.enum";
+import { IError } from '../interface/error.interface';
+
+export const successResponce = (code:HttpStatus,response:Response,data:any, alreadyWrappedWithData:boolean = false)=>{
+    let responseData = alreadyWrappedWithData ? data : {data};
+    response.status(code).json(responseData);
+}
+export const errorResponce = (code:HttpStatus,response:Response,error:IError)=>{
+    response.status(code).json({error});
+}
