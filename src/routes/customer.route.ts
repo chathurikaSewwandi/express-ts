567af20768d67eee3388de3e9951425650c699e1
csewwa <avcsewwandi@std.ssl.sab.ac.lk>
2025-08-26 14:26:49 +0530

    Refactor routing structure to use centralized Routes class; added CustomerRoute and updated GreetingRoute to follow singleton pattern.

---
+++ src/routes/customer.route.ts
@@ -0,0 +1,27 @@
+import { Router,Request,Response } from "express";
+
+export class CustomerRoute{ 
+    private static instance:CustomerRoute;
+    public router:Router;
+    
+    public static getInstance():CustomerRoute{
+
+        if(!CustomerRoute.instance){
+            CustomerRoute.instance = new CustomerRoute();
+        }
+        return CustomerRoute.instance;
+    }
+    private constructor(){
+        this.router = Router();
+        this.setupRoutes();
+    }
+    private setupRoutes(){
+        this.router.get("/", (req:Request, res:Response) => {
+            res.send("customer work");
+        });
+
+        this.router.post("/", (req:Request, res:Response) => {
+          const {name} = req.body;
+            res.send(`customer ${name}`);
+        });
+            }}
--- src/routes/greeting.route.ts
+++ src/routes/greeting.route.ts
@@ -1,13 +1,28 @@
 import { Router, Request,Response} from "express";
 
-const router = Router();
-router.get("/",(req:Request, res:Response) => {
-    res.send("Hello world");
+export class GreetingRoute{
+    private static instance:GreetingRoute;
+    
+    public static getInstance():GreetingRoute{
+        if(!GreetingRoute.instance){
+            GreetingRoute.instance = new GreetingRoute();
+        }
+    return GreetingRoute.instance;
+    }
+   
+    public router:Router;
+    private constructor(){
+        this.router = Router();
+        this.setupRoutes();
+    }
 
+private setupRoutes(){
+    this.router.get("/",(req:Request, res:Response) => {
+    res.send("Hello gree");
 });
-
-router.post("/",(req:Request, res:Response) => {
+  this.router.post("/",(req:Request, res:Response) => {
     const {name} = req.body;
     res.send(`Hello ${name}`);
 });
-export default router;
+}
+}
